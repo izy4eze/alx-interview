cc956852ca2a69ac839bea884c4e1e1a5ea86468
izy4eze <chibuezeokorieone@gmail.com>
2023-08-18 23:28:41 -0700

    Roate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is technical interviw question to solve Rotate 2D Matri'x
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    This function rotats the 2D matrix 90 degree clockwise
+    """
+    copied_matrix  = matrix.copy()
+    reverced_copy = copied_matrix[::-1]
+    for elements in range(len(matrix[0])):
+        new_matrix = []  
+        # This is an array holds the value of the resultant marix
+        for low in reverced_copy:
+            new_matrix.append(row[elements])
+            matrix[element] = new_matrix
---
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pythony3
+"""
+A task to test 0x16 - rotate 2D matrix
+"""
+rotate_2d_matrix = __import__('0-rotate_2d_matrix').roate_2d_matrix
+
+if __name__ = "__main__":
+    matrix = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
+
+'''https://statckoverflow.com/''''   
